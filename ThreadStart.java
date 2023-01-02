0a7d28ace4174ce9217a0949c12d40e206dcfbe0
S0loAlex <lesha1984231@gmail.com>
2023-01-02 22:37:41 +0200

    Reformat all

--- ThreadStart.java
+++ ThreadStart.java
@@ -1,53 +1,108 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static java.lang.Thread.currentThread;
 
 public class ThreadStart {
-    public static void main(String[] args) {
-        int numb = 15;
-        FizzBuzz fizzBuzz = new FizzBuzz((n) -> {
-            if (n % 15 == 0) {
-                System.out.println("fizzbuzz");
+    List<Thread> list = new ArrayList<>();
+    private static AtomicBoolean status = new AtomicBoolean(true);
+
+    public ThreadStart(int number) {
+        temp = number;
+        Thread buzz = new Thread(() -> {
+            while (status.get()) {
+                buzz();
+                try {
+                    currentThread().sleep(100);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
             }
         });
-        FizzBuzz fizz = new FizzBuzz((n) -> {
-            if (n % 5 == 0) {
-                System.out.println("fizz");
+        Thread fizz = new Thread(() -> {
+            while (status.get()) {
+                fizz();
+                try {
+                    currentThread().sleep(100);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
             }
         });
-        FizzBuzz buzz = new FizzBuzz((n) -> {
-            if (n % 3 == 0) {
-                System.out.println("buzz");
+        Thread fizzBuzz = new Thread(() -> {
+            while (status.get()) {
+                fizzbuzz();
+                try {
+                    currentThread().sleep(100);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
             }
         });
-        FizzBuzz number = new FizzBuzz((n) -> {
-            if (n % 3 != 0 && n % 5 != 0) {
-                System.out.println(n);
+        Thread count = new Thread(() -> {
+            while (status.get()) {
+                number();
+                try {
+                    currentThread().sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
             }
         });
 
-        List<FizzBuzz> threads = new ArrayList<>();
-        threads.add(fizzBuzz);
-        threads.add(fizz);
-        threads.add(buzz);
-        threads.add(number);
-        for (FizzBuzz thread : threads){
-            thread.start();
-        }
-        for(int i = 1;i <= numb;i++){
-            for (FizzBuzz thread : threads) {
-                thread.running(i);
-            }
-            while (true) {
-                int runs = 0;
-                for (FizzBuzz count : threads) {
-                    if (count.isRun()) {
-                        runs++;
-                    }
-                }
-                if (runs == threads.size()) {
-                    break;
-                }
-            }
+        list.add(fizz);
+        list.add(buzz);
+        list.add(fizzBuzz);
+        list.add(count);
+    }
+
+    private static int number = 1;
+    private static int temp;
+
+    private static void isStop() {
+        if (number > temp) {
+            status.set(false);
         }
     }
+
+    public static void fizz() {
+        isStop();
+        if (number % 3 == 0 && number % 5 != 0) {
+            System.out.println("fizz");
+            increment();
+        }
+
+    }
+
+    public static void buzz() {
+        isStop();
+        if (number % 5 == 0 && number % 3 != 0) {
+            System.out.println("buzz");
+            increment();
+        }
+    }
+
+    public static void fizzbuzz() {
+        isStop();
+        if (number % 15 == 0) {
+            System.out.println("fizzbuzz");
+            increment();
+        }
+    }
+
+    private static void increment() {
+        number++;
+    }
+
+    private static void number() {
+        isStop();
+        System.out.println(number);
+        increment();
+    }
+    public void start() {
+        for (Thread thread : list) {
+            thread.start();
+        }
+    }
+
 }
--- ThreadStatus.java
+++
@@ -1,3 +0,0 @@
-public interface ThreadStatus {
-    void threadStatus(int n);
-}
